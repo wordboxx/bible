20a14ee87b693af18001f2b1df33489f96418f41
= <=>
2025-03-01 15:25:25 -0600

    deleted/moved

--- utils/SysValidator.cpp
+++
@@ -1,7 +0,0 @@
-#include "../headers/SysValidator.h"
-#include <filesystem>
-
-int SysValidator::DirectoryValidator(std::string directoryPath) {
-  std::filesystem::directory_entry entry{directoryPath};
-  return entry.exists();
-}
