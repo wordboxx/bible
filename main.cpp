3e8a022cc45ed74eaaf907ef4964a7e71b3d22d7
kingdavid <honeybaptism@proton.me>
2025-03-05 03:10:19 -0600

    specified more filepaths

--- main.cpp
+++ main.cpp
@@ -3,15 +3,18 @@
 int main()
 {
 	// Filepath variables
+	// --- Data Directory
 	std::string dataDir = "data/";
 	// --- Unprocessed Bible
 	std::string unprocessedBibleFilename = "bibleTextNASB.txt";
 	std::string unprocessedBibleFilepath = dataDir + unprocessedBibleFilename;
 	// --- Processed Bible
 	std::string processedBibleFilepath = dataDir + "processedBible";
+	// --- Utility Files
+	std::string bibleBookNamesFilename = "bibleBookNames.txt";
+	std::string bibleBookNamesFilepath = dataDir + bibleBookNamesFilename;
 
-	// Process the Bible
-	BibleProcessor::ProcessBible(unprocessedBibleFilepath, processedBibleFilepath);
+	BibleProcessor::ScanBibleLines(unprocessedBibleFilepath);
 
 	return 0;
 }
