627c8bbd579457c3695a9a18f7063a5f029e909e
kingdavid <honeybaptism@proton.me>
2025-03-06 17:49:49 -0600

    refactor: update ScanBibleFile call to include line processing function

--- main.cpp
+++ main.cpp
@@ -8,13 +8,8 @@ int main()
 	// --- Unprocessed Bible
 	std::string unprocessedBibleFilename = "bibleTextNASB.txt";
 	std::string unprocessedBibleFilepath = dataDir + unprocessedBibleFilename;
-	// --- Processed Bible
-	std::string processedBibleFilepath = dataDir + "processedBible";
-	// --- Utility Files
-	std::string bibleBookNamesFilename = "bibleBookNames.txt";
-	std::string bibleBookNamesFilepath = dataDir + bibleBookNamesFilename;
 
-	BibleProcessor::ScanBibleFile(unprocessedBibleFilepath);
+	BibleProcessor::ScanBibleFile(unprocessedBibleFilepath, BibleProcessor::ProcessLine);
 
 	return 0;
 }
